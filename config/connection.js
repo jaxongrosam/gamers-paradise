9b1d183dd172233ad28414dec176b6bfd89ec171
jaxon grosam <jaxon.grosam@gmail.com>
2023-09-28 20:28:01 -0500

    creating new route to seed to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,21 @@ require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+sequelize = new Sequelize(
+  "mysql://p1da5qatqhyae1ko:athk7vq568tz0ow7@n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zqyn9ozyhneahp5o"
+);
+// if (process.env.JAWSDB_URL) {
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
